1c79049cf99ed23d6fd266aad1fd78c1b6c3b3eb
Jitu gandhare <jitugandhare@gmail.com>
2024-08-29 09:46:32 +0530

    lets begin

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,27 @@
 const express=require("express");
+const connection=require("./utils/db")
 const cors =require("cors");
 const cookieparser= require("cookie-parser");
+const dotenv=require("dotenv");
+dotenv.config()
 
 const app=express();
+app.get("/",(req,res)=>{
+    return res.status(200).json({
+        message:"data from backend",
+        success:true
+    })
+
+})
+app.use(express.json());
+app.use(cookieparser());
+app.use(express.urlencoded({extended:true}))
+app.use(cors());
+
+const corsOptions={
+    origin:"http://localhost:5173",
+    credentials:true
+}
 
 
 
@@ -12,7 +31,8 @@ const app=express();
 
 app.listen(8080,async()=>{
     try{
-        console.log("server is running on port 8080")
+        console.log(`server is running on port 8080`);
+        console.log("connected to DB");
     }catch(err){
         console.log(err)
     }
