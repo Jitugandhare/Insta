277008c6765c4185787d094155f02f9ca4066a37
Jitu gandhare <jitugandhare@gmail.com>
2024-09-10 12:21:59 +0530

    working on frontend start

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Label } from './ui/label';
 import { Input } from './ui/input';
 import { Button } from './ui/button';
+import axios from 'axios';
+import { toast } from 'sonner';
 
 const Signup = () => {
+
+    const [input, setInput] = useState({
+        username: "",
+        email: "",
+        password: ""
+    })
+
+    const changeEventHandler = (e) => {
+        setInput({ ...input, [e.target.name]: e.target.value });
+    }
+
+ const signupHandler=async(e)=>{
+    e.preventDefault();
+    console.log(input)
+    try{
+        const res=await axios.post("http://localhost:8080/user/register",input,{
+            headers:{
+                'Content-Type':'application/json'
+            },
+            withCredentials:true
+        });
+        if(res.data.success){
+            toast.success(res.data.message);
+        }
+
+    }catch(error){
+        console.log(error)
+        toast.error(error.response.data.message)
+    }
+ }
+
+
     return (
         <div className='flex items-center w-screen h-screen justify-center'>
-            <form action="" className='shadow-lg flex flex-col gap-5 p-8'>
+            <form onSubmit={signupHandler} className='shadow-lg flex flex-col gap-5 p-8'>
                 <div className='my-4 ' >
                     <h1 className='text-center font-bold text-xl'>Logo</h1>
                     <p className='text-sm text-center'>Signup to see photos & videos from your friends</p>
@@ -16,7 +50,9 @@ const Signup = () => {
                     <Input
                         type="text"
                         className="focus-visible:ring-transparent my-2"
-
+                        name="username"
+                        value={input.username}
+                        onChange={changeEventHandler}
 
                     />
                 </div>
@@ -25,21 +61,25 @@ const Signup = () => {
                     <Input
                         type="text"
                         className="focus-visible:ring-transparent my-2"
-
+                        name="email"
+                        value={input.email}
+                        onChange={changeEventHandler}
 
                     />
                 </div>
-                
+
                 <div>
                     <Label>Password</Label>
                     <Input
                         type="password"
                         className="focus-visible:ring-transparent my-2"
-
+                        name="password"
+                        value={input.password}
+                        onChange={changeEventHandler}
 
                     />
                 </div>
-                <Button>Signup</Button>
+                <Button type="submit">Signup</Button>
 
             </form>
         </div>
