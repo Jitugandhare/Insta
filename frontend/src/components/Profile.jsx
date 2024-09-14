e7b3600dbc2c087ac83a07e70770ddd3cf23848d
Jitu gandhare <jitugandhare@gmail.com>
2024-09-14 18:28:16 +0530

    right sidebar and suggested users done

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,18 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { useParams } from 'react-router-dom'
 import useGetUserProfile from '@/hooks/useGetUserProfile';
 import { useSelector } from 'react-redux';
 import { Button } from './ui/button';
+import { Badge } from './ui/badge';
+import { AtSign, Heart, MessageCircle } from 'lucide-react';
 
 const Profile = () => {
   const params = useParams();
   const userId = params.id;
+  const [activeTab, setActiveTab] = useState('posts')
   const { userProfile } = useSelector(store => store.auth);
   console.log(userProfile)
   useGetUserProfile(userId)
   const isLoggedUserProfile = true;
   const isFollowing = false;
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab)
+  }
+
+
+
+  const displayedPost = activeTab === 'posts' ? userProfile?.posts : userProfile?.bookmarks;
+
 
 
   return (
@@ -53,9 +65,61 @@ const Profile = () => {
                   )
                 }
               </div>
+              <div className='flex items-center gap-4'>
+                <p><span className='font-semibold'>{userProfile?.posts.length}</span> posts</p>
+                <p><span className='font-semibold'>{userProfile?.followers.length}</span> followers</p>
+                <p><span className='font-semibold'>{userProfile?.following.length}</span> following</p>
+              </div>
+              <div className='flex flex-col '>
 
+                <span>{userProfile?.bio || "bio here..."}</span>
+                <Badge className="w-fit" variant="secondary"><AtSign /><span className='pl-1'>{userProfile?.username}</span> </Badge>
+
+              </div>
             </div>
           </section>
+        </div>
+        <div className='border-t border-t-gray-200'>
+          <div className='flex items-center justify-center gap-10 text-sm' >
+            <span className={`py-3 cursor-pointer ${activeTab === 'posts' ? 'font-bold' : ""}`} onClick={() => handleTabChange('posts')}>
+              POSTS
+            </span>
+            <span className={`py-3 cursor-pointer ${activeTab === 'saved' ? 'font-bold' : ""}`} onClick={() => handleTabChange('saved')}>
+              SAVED
+            </span>
+            <span className='py-3 cursor-pointer'>
+              REELS
+            </span>
+            <span className='py-3 cursor-pointer'>
+              TAGS
+            </span>
+          </div>
+          <div className='grid grid-cols-3 gap-1'>
+            {
+              displayedPost?.map((post) => {
+                return (
+                  <div key={post?._id} className='relative group cursor-pointer'>
+                    <img src={post.image} alt="postimage" className='rounded-sm w-full object-cover aspect-square' />
+
+
+                    <div className='absolute inset-0 flex items-center justify-center bg-black bg-opacity-0 opacity-0 group-hover:bg-opacity-50 group-hover:opacity-100 transition-all duration-300'>
+                      <div className='flex items-center text-white space-x-4'>
+                        <button className="flex items-center gap-2 hover:text-gray-300 transition-colors duration-200">
+                          <Heart />
+                          <span>{post?.likes.length}</span>
+                        </button>
+                        <button className="flex items-center gap-2 hover:text-gray-300 transition-colors duration-200">
+                          <MessageCircle />
+                          <span>{post?.comments.length}</span>
+                        </button>
+                      </div>
+                    </div>
+
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
       </div>
 
