9c0354e016cac035efc2ff30884b46343881022e
Jitu gandhare <jitugandhare@gmail.com>
2024-09-15 23:54:47 +0530

    socket-io is setup and completed

--- frontend/src/hooks/useGetRTM.jsx
+++ frontend/src/hooks/useGetRTM.jsx
@@ -1,6 +1,4 @@
 import { setMessages } from "@/redux/chatSlice";
-import { setPosts } from "@/redux/postSlice";
-import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
@@ -8,16 +6,14 @@ const useGetRTM = () => {
     const dispatch = useDispatch();
     const { socket } = useSelector(store => store.socketio);
     const { messages } = useSelector(store => store.chat);
-
     useEffect(() => {
-        socket?.on("newMessage", (newMessage) => {
-            dispatch(setMessages([...messages, newMessage]))
+        socket?.on('newMessage', (newMessage) => {
+            dispatch(setMessages([...messages, newMessage]));
         })
 
         return () => {
-            socket?.off("newMessage")
+            socket?.off('newMessage');
         }
-
     }, [messages, setMessages]);
 };
 export default useGetRTM;
--- frontend/src/redux/rtnSlice.js
+++ frontend/src/redux/rtnSlice.js
@@ -1,19 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const rtnSlice = createSlice({
-    name:'realTimeNotification',
-    initialState:{
-        likeNotification:[], 
+    name: 'realTimeNotification',
+    initialState: {
+        likeNotification: [],
     },
-    reducers:{
-        setLikeNotification:(state,action)=>{
-            if(action.payload.type === 'like'){
+    reducers: {
+        setLikeNotification: (state, action) => {
+            if (action.payload.type === 'like') {
                 state.likeNotification.push(action.payload);
-            }else if(action.payload.type === 'dislike'){
-                state.likeNotification = state.likeNotification.filter((item)=> item.userId !== action.payload.userId);
+            } else if (action.payload.type === 'dislike') {
+                // Ensure the correct notification is removed
+                state.likeNotification = state.likeNotification.filter(
+                    (item) => item.userId !== action.payload.userId || item.postId !== action.payload.postId
+                );
             }
         }
     }
 });
-export const {setLikeNotification} = rtnSlice.actions;
+
+export const { setLikeNotification } = rtnSlice.actions;
 export default rtnSlice.reducer;
